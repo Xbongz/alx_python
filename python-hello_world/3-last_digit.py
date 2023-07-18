14bb770a50a987be6a6b15fc85c93bea733031c7
Bonginkosi <gbongig@gmail.com>
2023-07-18 20:23:46 +0200

    The last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -6,6 +6,6 @@ print("Last digit of", number, "is", last_digit, end=" ")
 if last_digit > 5:
     print("and is greater than 5")
 elif last_digit ==0:
-    print("last digit", number, "and is 0")
+    print("and is 0")
 else:
     print("and is less than 6 and not 0")
