04ff0e07f3de685da58afd8d4b2750ee0554e76d
Bonginkosi <gbongig@gmail.com>
2023-07-19 12:07:10 +0200

    updated to school

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-word = "Holberton"
+word = "School"
 slice = word[0:3]
 print("First 3 letters: {}".format(slice))
-slice = word[7:9]
+slice = word[4:7]
 print("Last 2 letters: {}".format(slice))
-slice = word[1:8]
+slice = word[1:6]
 print("Middle word: {}".format(slice))
