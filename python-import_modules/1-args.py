e87004cf5008095b11883211165dd9898160380c
Bonginkosi <gbongig@gmail.com>
2023-07-26 22:23:17 +0200

    How to make a script dynamic!

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -7,10 +7,10 @@ if __name__ == "__main__":
     argum = len(sys.argv) - 1
 #if no argument and print
     if argum == 0:
-        print("0 arguments:")
+        print("0 arguments.")
 #elif 1 argument and print
     elif argum == 1:
-        print("1 argument:")
+        print("1 argument.")
 #else there are more than 1 arguments and print
     else:
         print("{:d} arguments:".format(argum))
