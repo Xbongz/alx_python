6ed5880d5b614b3a5c3ca00d2725c8c5800934c3
Bonginkosi <gbongig@gmail.com>
2023-07-26 10:47:53 +0200

    Fibonacci Sequence Function

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -4,7 +4,10 @@ def fibonacci_sequence(n):
         return []
     elif n == 1:
         return [0]
+    elif n == 2:
+        return [0, 1]
     else:
         fibonacci = [0, 1]
         for i in range (2, n):
             fibonacci.append(fibonacci[i-1] + fibonacci[i-2])
+        return fibonacci
---
+++ python-functions/5-prime.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+def is_prime(number):
+    
