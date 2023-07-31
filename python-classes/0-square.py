2d9b4179aa67a91a668923b7ce0e213811f94b04
Bonginkosi <gbongig@gmail.com>
2023-07-31 20:47:16 +0200

    Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -3,7 +3,7 @@ class Square:
     def __init__(self, size):
         self.__size = size
 
-    square = Square(3)
-    print(type(square))
-    print(square.__dict__)
-    print(square._Square__size)
+square = Square(3)
+print(type(square))
+print(square.__dict__)
+print(square._Square__size)
