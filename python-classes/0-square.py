c43daa4fb924e657a31dc99432c586c99ae0dc7f
Bonginkosi <gbongig@gmail.com>
2023-08-02 21:14:50 +0200

    square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-"""The module defines class Square that describes the square by its size"""
+"""The module defines class Square that describes the square by its size."""
 
 class Square:
     """"To initialise the created square instance."""
    
     def __init__(self, size):
         """To initialise the square with size."""
-        self.__size = size # Assign private instance attribute
-    
-    def __str__(self):
-        """return string from the square"""
-        return "<class '{}'>\n{{'_{}__size': {}}}".format(self.__class__.__name__, self.__size)
-
-square = Square(3)
-print(square)
+        self.__size = size
