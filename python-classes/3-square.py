e299672c23167aa88a180e32c5ff01b8b5b57f4b
Bonginkosi <gbongig@gmail.com>
2023-08-03 00:29:00 +0200

    Access and update private attribute

---
+++ python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+"""A module for class that defines the area of a square with size=0"""
+
+class Square:
+    """initialising the created square instance"""
+    def __init__(self, size=0):
+        """For initialisation of square size"""
+        self.__size = size
+    
+    @property
+    def size(self):
+        """To get square size"""
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        """For setting the square size"""
+        
+        if not isinstance(value, int):
+            """Instance must be an integer if not must be a TypeError"""
+            raise TypeError("size must be an integer")
+        
+        elif value < 0:
+            """Instance for value must be less than zero if not there must be a valueError"""
+            raise ValueError("size must be >= 0")
+        
+        else:
+            self.__size = value
+
+    def area(self):
+        """public instance for the area"""
+        return self.__size * self.__size
