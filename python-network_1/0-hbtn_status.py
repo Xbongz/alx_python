aa8fa655ae92d0b89d4c72e5a68905196721e3b9
Bonginkosi <gbongig@gmail.com>
2023-08-31 14:10:01 +0200

    What's my status?

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -7,16 +7,3 @@ url = https://alu-intranet.hbtn.io/status
 
 """Send http get resquest"""
 response = requests.get(url)
-
-"""Check if response is successful"""
-if response.status_code == 200:
-    """Show response in tabulation"""
-    lines = response.text.split('\n')
-    
-    """To print lines"""
-    for line in lines:
-        print("\t{}".format(line))
-
-    """If the response fails to fecth"""
-else:
-    print("Did not fect URL. Status code: {}".format(response.status_code))
